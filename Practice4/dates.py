dcac8da4e976b7ff53a5e7757623f5522320da1b
KuanyshAsker <asker.k2006@gmail.com>
2026-02-28 11:09:37 +0500

    Add Practice4 - Python iterators, generators, dates, math, and JSON

---
+++ Practice4/dates.py
@@ -0,0 +1,19 @@
+import datetime
+x = datetime.datetime.now()
+print(x) # Import the datetime module and display the current date
+
+import datetime
+x = datetime.datetime.now()
+print(x.year)
+print(x.strftime("%A")) #Return the year and name of weekday
+
+import datetime
+x = datetime.datetime(2020, 5, 17)
+print(x) #Create a date object
+
+import datetime
+x = datetime.datetime(2018, 6, 1)
+print(x.strftime("%B")) #Display the name of the month:
+
+
+
---
+++ Practice4/math.py
@@ -0,0 +1,27 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y) #The min() and max() functions can be used to find the lowest or highest value in an iterable:
+
+x = abs(-7.25)
+print(x) #The abs() function returns the absolute (positive) value of the specified number:
+
+x = pow(4, 3)
+print(x) #Return the value of 4 to the power of 3 (same as 4 * 4 * 4):
+
+import math #import the math module
+
+import math
+x = math.sqrt(64)
+print(x) 
+
+import math
+x = math.ceil(1.4) #rounds a number upwards to its nearest integer
+y = math.floor(1.4) #rounds a number downwards to its nearest integer
+print(x) # returns 2
+print(y) # returns 1
+
+import math
+x = math.pi
+print(x)
+
